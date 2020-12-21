1fe8448d38f105d6c311666d4d54722130113e6d
doosemavis <moosedavis2011@gmail.com>
2020-12-21 14:02:28 -0600

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ function wrapAdjective(flair="*") {
       return `You are ${flair }${adj}${flair}!`;
     }
 }
+
+const Calculator = {
+    add: function(a,b) {
+        return a+b;
+    },
+    subtract: function(a,b) {
+        return a-b;
+    },
+    multiply: function(a,b) {
+        return a*b; 
+    },
+    divide: function(a,b) {
+        return a/b; 
+    }
+}
+
+function actionApplyer(start, finish) {
+    for (let i = 0; i < finish.length; i++) {
+        start = finish[i](start);
+    }
+    return start;
+}
