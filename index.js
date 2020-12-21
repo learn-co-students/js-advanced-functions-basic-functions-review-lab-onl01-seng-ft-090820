9a1d073dda774df4c48a223a91c37d14cc305079
doosemavis <moosedavis2011@gmail.com>
2020-12-21 14:04:05 -0600

    Done.

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ function wrapAdjective(flair="*") {
 
 const Calculator = {
     add: function(a,b) {
-        return a+b;
+        return a + b;
     },
     subtract: function(a,b) {
-        return a-b;
+        return a - b;
     },
     multiply: function(a,b) {
-        return a*b; 
+        return a * b; 
     },
     divide: function(a,b) {
-        return a/b; 
+        return a / b; 
     }
 }
 
