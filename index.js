10eb27c5633095840da4a194a2d03ea2bd49af74
brockbyrd <brockbyrdd@gmail.com>
2020-12-23 11:04:35 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+let saturdayFun = (activity = "roller-skate") => `This Saturday, I want to ${activity}!`
+
+let mondayWork = (activity = "go to the office") => `This Monday, I will ${activity}.`
+
+let wrapAdjective = (string = "*") => (feeling = "special") => `You are ${string}${feeling}${string}!`
+
+let Calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+}
+
+
+let actionApplyer = function(int, arr){
+    if(arr.length === 0){
+        return int
+    } else{      
+        for(const calc of arr){
+            int = calc(int)
+        }
+        return int
+    }
+}
